ceefaa4ef918f59d847b037ebfc7e6f172ac7c0a
roman-ra <romanra@posteo.net>
2022-12-04 17:49:26 +0100

    Add solution to the first problem

---
+++ day1/day1.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    std::ifstream input("input.txt");
+    std::string line;
+    
+    unsigned long long max_calories = 0;
+    unsigned long long current_calories = 0;
+
+    while (std::getline(input, line)) {
+        if (line.length() == 0) {
+            if (current_calories > max_calories) {
+                max_calories = current_calories;
+            }
+            current_calories = 0;
+        } else {
+            current_calories += std::stoull(line);
+        }
+    }
+    if (current_calories > max_calories) {
+        max_calories = current_calories;
+    }
+
+    std::cout<<max_calories<<std::endl;
+
+    return 0;
+}
